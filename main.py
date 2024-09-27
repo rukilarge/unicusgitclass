11f8ecc4b67605e2178f4981b208452b7f6cbd89
rukky <rukilargemartins@gmail.com>
2024-09-27 13:28:49 +0100

    This is the initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from utils import randomize_names
+
+
+if __name__ == "__main__":
+    print(__name__)
+    print(randomize_names())
---
+++ utils.py
@@ -0,0 +1,8 @@
+import random
+
+
+names = ["rukky", "emmanuel", "micaiah"]
+
+def randomize_names():
+    random.shuffle(names)
+    return names
